33fb0d946914eec4286175d71c7995b1ce52cc8c
Garth <garth.tutor@sincrodigital.com>
2021-07-01 13:32:54 -0700

    initial commit

--- php/src/BowlingKata.php
+++ php/src/BowlingKata.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-class Kata
+class BowlingKata
 {
 
 }
---
+++ php/tests/BowlingKataTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\BowlingKata;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+class BowlingKataTest extends TestCase
+{
+
+
+}
