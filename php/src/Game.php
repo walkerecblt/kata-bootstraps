aa598687eb1b299161576059520f735029031fb1
Robert Jones <Robert.Jones@cdk.com>
2021-07-06 10:46:54 -0700

    mob next [ci-skip] [ci skip] [skip ci]

--- php/src/Game.php
+++ php/src/Game.php
@@ -5,17 +5,24 @@ namespace App;
 class Game
 {
     private $score = 0 ;
-    private $frames = [];
-    private $currentFrame = 0;
+    private $rolls = [];
 
     public function scoreRoll(int $score)
     {
-        $this->frames[] = $score;
-        $this->score += $score;
+        $this->rolls[] = $score;
     }
 
     public function getScore() : int
     {
+        for (i = 0 ; i < count($this->rolls) ; ++i) {
+            if ($this->rolls[i])
+        }
+        foreach ($this->rolls as $roll) {
+            if($roll == 10) {
+                $this->score
+
+            }
+        }
         return $this->score;
     }
 }
